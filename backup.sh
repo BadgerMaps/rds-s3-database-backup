d64435b9bdc5b2e39446508f1cef88f278dc29ee
antoniogamizdelgado <antonio.gamiz@badgermapping.com>
2022-10-18 21:53:10 +0200

    Delete unused code
    The syntax VAR=${VAR:-`command`} is to provide a default value for VAR
    in case is not defined. In this case, the default value is being taken
    from SSM on AWS, but we are not using that service. We are providing
    those values as environment variables on Porter configuration so there
    is no need to have that code in the file.

--- backup.sh
+++ backup.sh
@@ -12,35 +12,25 @@ fi
 echo env: ${ENVIRONMENT}
 echo identifier: ${IDENTIFIER}
 
-DATABASE_HOST=${DATABASE_HOST:-`aws ssm get-parameter --region $REGION --name "/$ENVIRONMENT/cron/backup/$IDENTIFIER/host" --with-decrypt --query "Parameter.Value" --output text`}
-
 if [[ -z "${DATABASE_HOST}" ]]; then
   echo "Missing environment variable DATABASE_HOST"
   exit 1
 fi
-
-DATABASE_NAME=${DATABASE_NAME:-`aws ssm get-parameter --region $REGION --name "/$ENVIRONMENT/cron/backup/$IDENTIFIER/name" --with-decrypt --query "Parameter.Value" --output text`}
 
 if [[ -z "${DATABASE_NAME}" ]]; then
   echo "Missing environment variable DATABASE_NAME"
   exit 1
 fi
 
-DATABASE_USER=${DATABASE_USER:-`aws ssm get-parameter --region $REGION --name "/$ENVIRONMENT/cron/backup/$IDENTIFIER/user" --with-decrypt --query "Parameter.Value" --output text`}
-
 if [[ -z "${DATABASE_USER}" ]]; then
   echo "Missing environment variable DATABASE_USER"
   exit 1
 fi
 
-DATABASE_PASSWORD=${DATABASE_PASSWORD:-`aws ssm get-parameter --region $REGION --name "/$ENVIRONMENT/cron/backup/$IDENTIFIER/password" --with-decrypt --query "Parameter.Value" --output text`}
-
 if [[ -z "${DATABASE_PASSWORD}" ]]; then
   echo "Missing environment variable DATABASE_PASSWORD"
   exit 1
 fi
-
-S3_BUCKET=${S3_BUCKET:-`aws ssm get-parameter --region $REGION --name "/$ENVIRONMENT/cron/backup/$IDENTIFIER/bucket" --with-decrypt --query "Parameter.Value" --output text`}
 
 if [[ -z "${S3_BUCKET}" ]]; then
   echo "Missing environment variable S3_BUCKET"
